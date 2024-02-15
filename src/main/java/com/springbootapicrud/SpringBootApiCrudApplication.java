db245f200f9556df028bb1190be9b93eb60e97e6
Wagner Coche <wagnercoche@gmail.com>
2024-02-15 17:47:41 +0100

    first commit

---
+++ src/main/java/com/springbootapicrud/SpringBootApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.springbootapicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootapicrud/SpringBootApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
